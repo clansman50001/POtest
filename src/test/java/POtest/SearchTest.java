e12f5bcf7db7d1a5c1be3d40adda26ec30d88714
Valentin Andreev <onuswarm@gmail.com>
2018-07-19 14:57:55 +0300

    BaseTest corrected-2

--- src/test/java/POtest/SearchTest.java
+++ src/test/java/POtest/SearchTest.java
@@ -10,6 +10,6 @@ public class SearchTest extends BaseTest {
     public void poTest() {
         homePage.search("пенза погода");
         String weather = resultPage.getResult();
-        Assert.assertEquals("Погода  Пенза", weather);
+        Assert.assertEquals("Погода в Пензе", weather);
     }
 }
